c37b65c42c143ba6eef0f9786c5daefb30419d9a
Nancy Rankin <nan2059153@maricopa.edu>
2024-11-13 20:06:51 -0700

    Initial commit

---
+++ guess_the_word.py
@@ -0,0 +1,7 @@
+#Nancy Rankin
+#CIS256 Fall 2024
+# EX 4 Testing with pytest and Git Manipulation
+
+import random
+word_dic = ["phase" "magic", "flash", "globe", "usage", "shall", "unity", "drove", "water", "sharp"]
+
