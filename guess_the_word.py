d1e9611f418c6e054b05291b187d5acb4497bed2
Nancy Rankin <nan2059153@maricopa.edu>
2024-11-13 20:50:35 -0700

    Updated guess_the_word.py

--- guess_the_word.py
+++ guess_the_word.py
@@ -5,3 +5,29 @@
 import random
 word_dic = ["phase" "magic", "flash", "globe", "usage", "shall", "unity", "drove", "water", "sharp"]
 
+print("Welcome to Guess the Word! In this program you will get 25 chances to guess a 5 letter word")
+
+word_to_guess = random.choice(word_dic)
+letter1 = word_to_guess[0]
+letter2 = word_to_guess[1]
+letter3 = word_to_guess[2]
+letter4 = word_to_guess[3]
+letter5 = word_to_guess[4]
+user_guess = ""
+guesses = 25
+for letter in word_to_guess:
+    if guess == letter1:
+        user_guess.append(letter1)
+
+    if guess == letter2:
+        user_guess.append(letter2)
+
+    if guess == letter3:
+        user_guess.append(letter3)
+
+    if guess == letter4:
+        user_guess.append(letter4)
+
+    if guess == letter5:
+        user_guess.append(letter5)
+
