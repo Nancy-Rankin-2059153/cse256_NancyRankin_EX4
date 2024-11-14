44f789dba2eedcb065b634926ddedcc98f070482
Nancy Rankin <nan2059153@maricopa.edu>
2024-11-13 21:40:37 -0700

    Update Test_guess_the_word changing to pytest and 1st test for word list

--- Test_guess_the_word.py
+++ Test_guess_the_word.py
@@ -1,8 +1,14 @@
-import unittest
+import pytest
+
+import guess_the_word
+from guess_the_word import word_to_guess, word_dic
+
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    def test_wor_in_list():
+        assert word_to_guess in word_dic
+        
+
 
 if __name__ == '__main__':
     unittest.main()
